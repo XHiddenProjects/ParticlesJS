1db8d318eeb3016ef5f3bff0cb3214fcb039f992
XHiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2026-01-05 10:08:15 -0500

    Delete demo.js

--- demo.js
+++
@@ -1,6 +0,0 @@
-if(window.ParticleJS){
-    const particles = new ParticleJS('#hero');
-    particles.setScene(ParticleJS.Particles.rain);
-    particles.play();
-
-}else throw new Error('ParticleJS has not been loaded');
